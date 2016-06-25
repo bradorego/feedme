86ab02f9890f950c8c89f24f08ab5cc8e3baf2a3
Brad Orego <me@bradorego.com>
2016-06-24 23:00:36 -0500

    adding eat street library...?

--- www/js/-compiled/all.js
+++ www/js/-compiled/all.js
@@ -83,6 +83,26 @@
     .service('EatStreet', ESFact);
 }());
 
+// User.js
+(function () {
+  'use strict';
+  var UserFact = [
+
+    function () {
+      var User = {};
+
+      User.create = function (object) {
+
+      };
+
+      return User;
+    }
+  ];
+
+  angular.module('feed-me')
+    .service('User', UserFact);
+}());
+
 /// home.Home.js
 (function () {
   'use strict';
---
+++ www/js/services/User.js
@@ -0,0 +1,19 @@
+// User.js
+(function () {
+  'use strict';
+  var UserFact = [
+
+    function () {
+      var User = {};
+
+      User.create = function (object) {
+
+      };
+
+      return User;
+    }
+  ];
+
+  angular.module('feed-me')
+    .service('User', UserFact);
+}());
